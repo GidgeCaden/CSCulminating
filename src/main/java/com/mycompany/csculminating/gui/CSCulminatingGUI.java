e7e0a012d3ba48887ddf548010d0b211801fc909
CadenG <cadengidge@gmail.com>
2025-05-29 10:46:57 -0400

    e

---
+++ src/main/java/com/mycompany/csculminating/gui/CSCulminatingGUI.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.csculminating.gui;
+
+/**
+ *
+ * @author CaGid4274
+ */
+public class CSCulminatingGUI {
+
+    public static void main(String[] args) {
+        GUI test = new GUI();
+        test.setVisible(true);
+    }
+}
